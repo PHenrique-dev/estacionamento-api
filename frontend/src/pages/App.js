4678c8e2f317f1765bbb74f460850405e6bf1f6d
PHenrique-dev <pedrinho03s4mini@gmail.com>
2023-11-29 19:51:51 -0300

    Organizando

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,26 +1,64 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import LoginPage from './LoginPage';
 
 const App = () => {
-  const [token, setToken] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const login = async (email, password) => {
+    try {
+      const response = await axios.post('/auth/user', { email, password });
+      const token = response.data.token;
+      localStorage.setItem('token', token);
+      setIsLoggedIn(true);
+      setUser(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const register = async (user) => {
+    try {
+      const response = await axios.post('/auth/register', user);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
-    const tokenFromLocalStorage = localStorage.getItem('token');
-    if (tokenFromLocalStorage) {
-      setToken(tokenFromLocalStorage);
+    if (localStorage.getItem('token')) {
+      setIsLoggedIn(true);
+      const token = localStorage.getItem('token');
+      const response =  axios.get('/auth/user', { headers: { Authorization: `Bearer ${token}` } });
+      setUser(response.data);
     }
   }, []);
 
   return (
-    <div className="app">
-      {token && (
+    <div>
+      {isLoggedIn && (
         <div>
-          <h1>Bem-vindo!</h1>
-          <button onClick={() => setToken('')}>Logout</button>
+          <h1>Usuário logado</h1>
+          <p>Nome: {user.name}</p>
+          <p>Email: {user.email}</p>
+          <button onClick={() => setIsLoggedIn(false)}>Logout</button>
         </div>
       )}
-      {!token && <LoginPage />}
+      {!isLoggedIn && (
+        <div>
+          <h1>Login</h1>
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            login(e.target.email.value, e.target.password.value);
+          }}>
+            <input type="email" name="email" placeholder="Endereço de email" />
+            <input type="password" name="password" placeholder="Senha" />
+            <button type="submit" href="#">Login</button>
+          </form>
+          <p>Não tem conta? <a href="/auth/register">Registre-se</a></p>
+        </div>
+      )}
     </div>
   );
 };
--- frontend/src/services/ApiServices.js
+++ frontend/src/services/ApiServices.js
@@ -1,6 +1,111 @@
-import axios from "axios"
-const API_URL = process.env.REACT_APP_API_URL
-
-export async function getFunction(){
-    const response = await axios.get(`${API_URL}/`)
-} 
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+const instance = axios.create({
+  baseURL: 'http://localhost:3000/api', 
+});
+const getUsers = async () => {
+    try {
+      const response = await instance.get('/users');
+      const users = response.data;
+      console.log(users);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const register = async (user) => {
+    try {
+      const response = await instance.post('/auth/register', user);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  const login = async (email, password) => {
+    try {
+      const response = await instance.post('/auth/user', { email, password });
+      const token = response.data.token;
+      localStorage.setItem('token', token);
+      console.log('Logado com sucesso!');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+  const checkToken = (req, res, next) => {
+    const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    try {
+      const decoded = jwt.verify(token, process.env.SECRET);
+      req.userId = decoded.id;
+      next();
+    } catch (error) {
+      return res.status(400).json({ message: 'Invalid token' });
+    }
+  };
+  const App = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [user, setUser] = useState(null);
+  
+    const login = async (email, password) => {
+      try {
+        const response = await axios.post('/auth/user', { email, password });
+        const token = response.data.token;
+        localStorage.setItem('token', token);
+        setIsLoggedIn(true);
+        setUser(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    const register = async (user) => {
+      try {
+        const response = await axios.post('/auth/register', user);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    useEffect(() => {
+      if (localStorage.getItem('token')) {
+        setIsLoggedIn(true);
+        const token = localStorage.getItem('token');
+        const response = await axios.get('/auth/user', { headers: { Authorization: `Bearer ${token}` } });
+        setUser(response.data);
+      }
+    }, []);
+  
+    return (
+      <div>
+        {isLoggedIn && (
+          <div>
+            <h1>Usuário logado</h1>
+            <p>Nome: {user.name}</p>
+            <p>Email: {user.email}</p>
+            <button onClick={() => setIsLoggedIn(false)}>Logout</button>
+          </div>
+        )}
+        {!isLoggedIn && (
+          <div>
+            <h1>Login ou registro</h1>
+            <form onSubmit={(e) => {
+              e.preventDefault();
+              login(e.target.email.value, e.target.password.value);
+            }}>
+              <input type="email" name="email" placeholder="Endereço de email" />
+              <input type="password" name="password" placeholder="Senha" />
+              <button type="submit">Login</button>
+            </form>
+            <p>Não tem conta? <a href="#">Registre-se</a></p>
+          </div>
+        )}
+      </div>
+    );
+  };
+  
+  export default App;
+  
