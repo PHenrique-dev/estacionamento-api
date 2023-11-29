e98e74fd90c4c3053f951fb73ec7abe1f2162d5c
PHenrique-dev <pedrinho03s4mini@gmail.com>
2023-11-29 14:51:56 -0300

    Criando o front

--- src/index.js
+++ src/index.js
@@ -34,14 +34,14 @@ function checkToken(req, res, next){
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(" ")[1]
     if(!token){
-        return res.sendStatus(401).json({msg: "Acesso negado"})
+        return res.status(401).json({msg: "Acesso negado"})
     }
     try {
         const secret = process.env.SECRET
         jwt.verify(token, secret)
         next()
     } catch (error) {
-        return res.sendStatus(400).json({msg: "Token inválido"})
+        return res.status(400).json({msg: "Token inválido"})
     }
 }
 app.post("/auth/register", async(req, res) =>{
