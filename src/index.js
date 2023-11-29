104954deeb0367ab522f55affaa391c9650004ef
PHenrique-dev <pedrinho03s4mini@gmail.com>
2023-11-29 11:15:57 -0300

    Criação do user

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const app = express();
 const cors = require("cors")
+const User = require('./models/User.js')
 app.use(cors())
 app.use(express.json())
 const conn = require("./config/conn.js")
@@ -21,3 +22,19 @@ app.use("/api", routes)
 app.get("/", (req, res) =>{
     res.json({mensagem: 'API rodando!'})
 });
+app.post("/auth/register", async(req, res) =>{
+    const{ name, email, password, confirmpassword } = req.body
+    if(!name){
+        return res.status(422).json({msg: "O nome é obrigatório"})
+    }
+    if(!email){
+        return res.status(422).json({msg: "O email é obrigatório"})
+    }
+    if(!password){
+        return res.status(422).json({msg: "A senha é obrigatória"})
+    }
+    if(password !== confirmpassword){
+        return res.status(422).json({msg: "As senhas não são iguais"})
+    }
+    
+})
